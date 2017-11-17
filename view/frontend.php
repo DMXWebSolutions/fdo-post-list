efd4f91e16e82acd748c5299bed6c367c80e6895
Cleiton Pereira <cleiton.workspace@gmail.com>
2017-11-17 17:31:14 -0200

    Ajustes no front

--- view/frontend.php
+++ view/frontend.php
@@ -4,11 +4,9 @@
             <?= $instance['title'] ?>
         <?= $after_title ?>
     <?php endif; ?>
-    <div class="fdo-posts-list-widget">
-        <ul class="fdo-posts-list">
-            <?php foreach($posts as $post): ?>
-                <li class="fdo-post-item"><a class="post-link" href="<?= get_permalink($post->ID); ?>"><?= $post->post_title ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+    <ul class="fdo-posts-list">
+        <?php foreach($posts as $post): ?>
+            <li class="fdo-post-item"><a class="post-link" href="<?= get_permalink($post->ID); ?>"><?= $post->post_title ?></a></li>
+        <?php endforeach; ?>
+    </ul>
 <?= $after_widget ?>
